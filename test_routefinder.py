cdba7f387cd133c90356b7e5e5df5d21559b299e
Andrew <adiep@dons.usfca.edu>
2024-09-14 18:04:10 -0700

    q3 v4

--- test_routefinder.py
+++ test_routefinder.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from routefinder import *
+from search_algorithms import sld
 
 class Testmap_state(TestCase):
     def test_is_lt (self) :
@@ -9,7 +10,12 @@ class Testmap_state(TestCase):
         self.assertLessEqual(s1,s2)
 
 
-    def test_sld(self) :
+    def test_sld_a(self) :
         s1 = map_state(g = 1,h=1)
         val = sld(s1)
         self.assertLessEqual(val, 14)
+
+    def test_sld_b(self) :
+        s1 = map_state(location="25,1", g = 1,h=1)
+        val = sld(s1)
+        self.assertGreater(val, 14)
